0e6b1131828716bccf0e3cab811e27726f95f4d9
FarahR01 <farahr2001@gmail.com>
2026-03-26 01:30:26 +0100

    feat: implement core App component with global styling
    - Create main App component with routing structure
    - Define global CSS variables and base styles
    - Set up index CSS for layout and typography foundations
    - Establish responsive design baseline

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import ExercisesShowcase from "./pages/ExercisesShowcase";
+import PatternsShowcase from "./pages/PatternsShowcase";
 import ProfilePage from "./pages/ui-exercices/ProfilePage";
 import NavbarPage from "./pages/ui-exercices/NavbarPage";
 import TodoPage from "./pages/ui-exercices/TodoPage";
 import ProductPage from "./pages/ui-exercices/ProductPage";
 import DashboardPage from "./pages/ui-exercices/DashboardPage";
 import FormPage from "./pages/ui-exercices/FormPage";
+import AccordionPage from "./pages/advanced-react-patterns/AccordionPage";
+import RenderPropsPage from "./pages/advanced-react-patterns/RenderPropsPage";
+import TabsPatternPage from "./pages/advanced-react-patterns/TabsPatternPage";
 import "./App.css";
+import LearnPage from "./pages/LearnPage";
 
 function AppContent() {
   return (
@@ -15,11 +21,14 @@ function AppContent() {
       <header className="app-header">
         <div className="app-header__container">
           <Link to="/" className="app-header__logo">
-            React UI Exercises 🚀
+            React Mastery 🚀
           </Link>
           <nav className="app-header__nav">
             <Link to="/" className="app-header__link">
               Home
+            </Link>
+            <Link to="/learn" className="app-header__link">
+              Learn
             </Link>
             <a
               href="https://github.com"
@@ -36,13 +45,19 @@ function AppContent() {
       {/* Routes */}
       <main className="app-main">
         <Routes>
-          <Route path="/" element={<ExercisesShowcase />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/learn" element={<LearnPage />} />
+          <Route path="/ui-exercises" element={<ExercisesShowcase />} />
+          <Route path="/patterns" element={<PatternsShowcase />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/navbar" element={<NavbarPage />} />
           <Route path="/todo" element={<TodoPage />} />
           <Route path="/product" element={<ProductPage />} />
           <Route path="/form" element={<FormPage />} />
+          <Route path="/patterns/accordion" element={<AccordionPage />} />
+          <Route path="/patterns/render-props" element={<RenderPropsPage />} />
+          <Route path="/patterns/tabs" element={<TabsPatternPage />} />
         </Routes>
       </main>
     </>
